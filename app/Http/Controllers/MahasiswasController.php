f942ba6626370c9499fa993f319884fb445eecf5
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-09-18 21:10:01 +0700

    create mahasiswa model, controller, migration, seeder
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

---
+++ app/Http/Controllers/MahasiswasController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswasController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Mahasiswa $mahasiswa)
+    {
+    }
+
+    public function edit(Mahasiswa $mahasiswa)
+    {
+    }
+
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+    }
+
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+    }
+}
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -9,66 +9,106 @@
 @stop
 
 @section('content')
-    <div class="col-xl-6 card" style="padding: 2em">
-        <div class="card-body">
-            <form action="{{ route('post.admin.profile.update', $user->id) }}" method="POST">
-                @csrf
-                @if(session('errors'))
-                    <x-adminlte-alert theme="danger" title="Error!" dismissable>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </x-adminlte-alert>
-                @endif
-                @if (Session::has('success'))
-                    <div class="alert alert-success m-b-16" role="alert">
-                        {{ Session::get('success') }}
-                    </div>
-                @endif
-                @if (Session::has('error'))
-                    <div class="alert alert-danger m-b-16" role="alert">
-                        {{ Session::get('error') }}
-                    </div>
-                @endif
-                <x-adminlte-input name="name" label="{{ trans('auth.nama') }}"
-                                  placeholder="{{ trans('auth.nama') }}" value="{{ $user->name }}">
-                    <x-slot name="prependSlot">
-                        <div class="input-group-text">
-                            <i class="fas fa-user text-green"></i>
-                        </div>
-                    </x-slot>
-                </x-adminlte-input>
-                <x-adminlte-input name="email" type="email" label="{{ trans('auth.text_email') }}"
-                                  placeholder="{{ trans('auth.text_email') }}" value="{{ $user->email }}">
-                    <x-slot name="prependSlot">
-                        <div class="input-group-text">
-                            <i class="fas fa-mail-bulk text-lightblue"></i>
-                        </div>
-                    </x-slot>
-                </x-adminlte-input>
-                <x-adminlte-input name="password" type="password" label="{{ trans('auth.text_password') }}"
-                                  placeholder="{{ trans('auth.text_password') }}">
-                    <x-slot name="prependSlot">
-                        <div class="input-group-text">
-                            <i class="fas fa-key text-lightblue"></i>
-                        </div>
-                    </x-slot>
-                </x-adminlte-input>
-                <x-adminlte-input name="password_confirmation" type="password" label="{{ trans('auth.konfirmasi_password') }}"
-                                  placeholder="{{ trans('auth.konfirmasi_password') }}">
-                    <x-slot name="prependSlot">
-                        <div class="input-group-text">
-                            <i class="fas fa-key text-lightblue"></i>
-                        </div>
-                    </x-slot>
-                </x-adminlte-input>
-                <x-adminlte-button class="btn-primary" type="submit" label="{{ trans('auth.simpan') }}" theme="success"
-                                   icon="fas fa-lg fa-save"/>
-            </form>
+    <form action="{{ route('post.admin.profile.update', $user->id) }}" method="POST">
+        <div class="row">
+            @csrf
+            <div class="col-xl-6">
+                <x-adminlte-card title="Basic" icon="fas fa-lg fa-id-card">
+                    @if(session('errors'))
+                        <x-adminlte-alert theme="danger" title="Error!" dismissable>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </x-adminlte-alert>
+                    @endif
+                    @if (Session::has('success'))
+                        <x-adminlte-alert theme="success" title="Success!" dismissable>
+                            {{ Session::get('success') }}
+                        </x-adminlte-alert>
+                    @endif
+                    @if (Session::has('error'))
+                        <x-adminlte-alert theme="danger" title="Error!" dismissable>
+                            {{ Session::get('error') }}
+                        </x-adminlte-alert>
+                    @endif
+                    <x-adminlte-input name="name" label="{{ trans('auth.nama') }}"
+                                      placeholder="{{ trans('auth.nama') }}" value="{{ $user->name }}">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text">
+                                <i class="fas fa-user text-lightblue"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                    <x-adminlte-input name="email" type="email" label="{{ trans('auth.text_email') }}"
+                                      placeholder="{{ trans('auth.text_email') }}" value="{{ $user->email }}">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text">
+                                <i class="fas fa-mail-bulk text-lightblue"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                    <x-adminlte-input name="password" type="password" label="{{ trans('auth.text_password') }}"
+                                      placeholder="{{ trans('auth.text_password') }}">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text">
+                                <i class="fas fa-key text-lightblue"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                    <x-adminlte-input name="password_confirmation" type="password"
+                                      label="{{ trans('auth.konfirmasi_password') }}"
+                                      placeholder="{{ trans('auth.konfirmasi_password') }}">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text">
+                                <i class="fas fa-key text-lightblue"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                    <x-adminlte-button class="btn-primary" type="submit" label="{{ trans('auth.simpan') }}"
+                                       theme="success"
+                                       icon="fas fa-lg fa-save"/>
+                </x-adminlte-card>
+            </div>
+            @if ($user->mahasiswa)
+                <div class="col-xl-6">
+                    <x-adminlte-card title="Data Mahasiswa" icon="fas fa-lg fa-id-card">
+                        <x-adminlte-callout theme="info" class="bg-gradient-info" title="Informasi">
+                            Jika ingin mengganti data mahasiswa, silahkan hubungi admin.
+                        </x-adminlte-callout>
+                        <x-adminlte-input name="nim" label="{{ trans('auth.nim') }}"
+                                          placeholder="{{ trans('auth.nim') }}" value="{{ $user->mahasiswa->nim }}"
+                                          disabled>
+                            <x-slot name="prependSlot">
+                                <div class="input-group-text">
+                                    <i class="fas fa-id-card text-lightblue"></i>
+                                </div>
+                            </x-slot>
+                        </x-adminlte-input>
+                        <x-adminlte-input name="prodi" label="{{ trans('auth.prodi') }}"
+                                          placeholder="{{ trans('auth.prodi') }}" value="{{ $user->mahasiswa->prodi }}"
+                                          disabled>
+                            <x-slot name="prependSlot">
+                                <div class="input-group-text">
+                                    <i class="fas fa-id-card-alt text-lightblue"></i>
+                                </div>
+                            </x-slot>
+                        </x-adminlte-input>
+                        <x-adminlte-input name="jurusan" label="{{ trans('auth.jurusan') }}"
+                                          placeholder="{{ trans('auth.jurusan') }}"
+                                          value="{{ $user->mahasiswa->jurusan }}" disabled>
+                            <x-slot name="prependSlot">
+                                <div class="input-group-text">
+                                    <i class="fas fa-id-badge text-lightblue"></i>
+                                </div>
+                            </x-slot>
+                        </x-adminlte-input>
+                    </x-adminlte-card>
+                </div>
+            @endif
         </div>
-    </div>
+    </form>
 @endsection
 
 @section('css')
