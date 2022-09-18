8b111f4fa38cf93908149b187ed624e1beb7aa11
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-09-18 18:30:00 +0700

    change favicon, logo and admin logo name
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,8 +63,8 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>Panel',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => 'SPKKUIAINP',
+    'logo_img' => 'logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
@@ -253,7 +253,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'admin',
+    'dashboard_url' => 'home',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
